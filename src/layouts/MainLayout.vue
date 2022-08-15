599b217975d93ed56733bff2d9530ab63ec91181
Mohammed <mumo7038@gmail.com>
2022-08-16 01:58:30 +0800

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,65 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header reveal class="text-white q-py-xs">
+      <q-toolbar>
+        <q-btn  class="bg-white" flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-space />
+        <q-avatar square size="80px">
+          <img contain src="../assets/ibex-l-y-01.png" />
+        </q-avatar>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" side="left" overlay behavior="desktop">
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer reveal class="text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div class="row justify-end">
+            <small class=""> &COPY; IEBX FURNITURE </small>
+          </div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.q-header {
+  background-color: $G;
+}
+.q-footer {
+  background-color: $G;
+}
+small {
+  font-family: sans-serif;
+  letter-spacing: 1px;
+  font-size: 14px;
+}
+::v-deep.q-btn .q-icon {
+  color: $Y;
+  font-size: 28.2px;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-page class="row items-center justify-start">
+    <q-avatar square size="500px">
+      <q-img src="../assets/ibex-l-g-01.png" contain spinner-color="red" />
+    </q-avatar>
+    <p>Coming soon</p>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+<style lang="scss" scoped>
+.q-page {
+  background-color: $Y;
+  flex-direction: column;
+  font-family: sans-serif;
+}
+p {
+  font-family: serif;
+  font-size: 25px;
+  letter-spacing: 1.5px;
+  font-weight: 700;
+}
+</style>
